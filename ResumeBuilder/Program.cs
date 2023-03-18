ca1a1b9f17e58e8de3ec006107aa37a2c9a06d9e
samet <57061347+sametcn99@users.noreply.github.com>
2023-03-18 19:31:04 +0300

    Update Program.cs

--- ResumeBuilder/Program.cs
+++ ResumeBuilder/Program.cs
@@ -6,6 +6,12 @@ namespace ResumeBuilder
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
+
+        /*This code is the entry point for a Windows Forms application that builds resumes. 
+        It initializes the application configuration and runs the login form. The ApplicationConfiguration.Initialize() 
+        method is used to customize the application configuration, such as setting high DPI settings or default font. 
+        The FormLogin is the first form that the user sees when they launch the application, and it prompts the user to 
+        log in before they can access the resume builder.*/
         static void Main()
         {
             // To customize application configuration such as set high DPI settings or default font,
